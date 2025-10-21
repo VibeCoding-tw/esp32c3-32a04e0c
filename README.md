44b28096a2743c42c7eaf7c331ec58b6048af5d5
roverchen <rover.k.chen@gmail.com>
2025-10-21 22:17:08 +0800

    src/main.cpp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # esp32c3-32a04e0c
+Orange 08 Shuttel: esp32c3-0c4ea032119c
+四驅攀爬車：軟輪：esp32c3-0c4ea032ad7c
---
+++ include/gpio_pins.h
@@ -0,0 +1,6 @@
+// --- 馬達驅動晶片 GPIO 設定 (Motor H-Bridge DRV8833) ---
+const int motorA_pwm_fwd = 6;
+const int motorA_pwm_rev = 5;
+const int motorB_pwm_left  = 20;
+const int motorB_pwm_right = 21;
+const int motor_stby = 7; // 設定 HIGH 啟用馬達
